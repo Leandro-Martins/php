dda6b975a021f8795fe601f5412e59e0b60a9019
pagseguro <pagseguro@visie.com.br>
2010-01-08 17:29:38 -0200

    Criando funcao para rodar o arquivo sozinho

---
+++ tests/config.php
@@ -0,0 +1,16 @@
+<?php
+
+function rodaTest($class)
+{
+    if (count($_SERVER['argv'])) {
+        require_once('PHPUnit/Framework.php');
+        require_once('PHPUnit/TextUI/ResultPrinter.php');
+
+        $suite = new PHPUnit_Framework_TestSuite();
+        $suite->addTestSuite($class);
+        $result = $suite->run();
+        $reporter = new PHPUnit_TextUI_ResultPrinter;
+        $reporter->printResult($result);
+    }
+}
+
