9b42b8516b55bb68b668583f68ffe595548878f0
Michael Granados <michaelgranados@gmail.com>
2010-04-29 23:18:19 -0300

    Adicionando codigos dos Correios

--- Frete.php
+++ Frete.php
@@ -4,4 +4,16 @@
 class Pagseguro_Frete
 {
     // http://shopping.correios.com.br/wbm/shopping/script/CalcPrecoPrazo.aspx?StrRetorno=xml&nCdServico=40010&nVlPeso=1&sCepOrigem=[00000000]&sCepDestino=00000000&nCdFormato=1&sCdMaoPropria=N&sCdAvisoRecebimento=N&nVlValorDeclarado=0
+    
+    ####################################
+    # Código dos Serviços dos Correios #
+    #    FRETE PAC = 41106             #
+    #    FRETE SEDEX = 40010           #
+    #    FRETE SEDEX 10 = 40215        #
+    #    FRETE SEDEX HOJE = 40290      #
+    #    FRETE E-SEDEX = 81019         #
+    #    FRETE MALOTE = 44105          #
+    #    FRETE NORMAL = 41017          #
+    #   SEDEX A COBRAR = 40045         #
+    ####################################
 }
