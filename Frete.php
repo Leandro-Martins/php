8f4b2a34c54b9e097f6602716d4451f9c33c6772
Michael <mike@visie.com.br>
2010-06-16 16:08:40 -0300

    começando a montar a url necessaria dos correios

--- Frete.php
+++ Frete.php
@@ -3,28 +3,18 @@
 
 class Pagseguro_Frete
 {
-    // http://shopping.correios.com.br/wbm/shopping/script/CalcPrecoPrazo.aspx
-    // ?StrRetorno=xml
-    // &nCdServico=40010
-    // &nVlPeso=1
-    // &sCepOrigem=00000000
-    // &sCepDestino=00000000
-    // &nCdFormato=1
-    // &sCdMaoPropria=N
-    // &sCdAvisoRecebimento=N
-    // &nVlValorDeclarado=0
+    static private $urlCorreios = 'http://shopping.correios.com.br/wbm/shopping/script/CalcPrecoPrazo.aspx';
 
-    ####################################
-    # Código dos Serviços dos Correios #
-    #    FRETE PAC        = 41106      #
-    #    FRETE SEDEX      = 40010      #
-    #    FRETE SEDEX 10   = 40215      #
-    #    FRETE SEDEX HOJE = 40290      #
-    #    FRETE E-SEDEX    = 81019      #
-    #    FRETE MALOTE     = 44105      #
-    #    FRETE NORMAL     = 41017      #
-    #    SEDEX A COBRAR   = 40045      #
-    ####################################
+    static private $servicosCorreios = array(
+        'pac'          => 41106,
+        'sedex'        => 40010,
+        'sedex10'      => 40215,
+        'sedexhoje'    => 40290,
+        'esedex'       => 81019,
+        'malote'       => 44105,
+        'normal'       => 41017,
+        'sedexacobrar' => 40045,
+    );
 
     public $source  = array('pagseguro', 'correios'); // pagseguro, correios
     public $origem  = null;
@@ -32,4 +22,22 @@ class Pagseguro_Frete
     public $peso    = 0; // Em gramas: 300 equivale a 3000 gramas. 1000 = 1Kg
     public $valor   = 0; // Valor declarado
     public $tipo    = array('sedex', 'pac'); // sedex, pac, sedex10, sedexhoje, esedex, malote, normal, sedexacobrar - nota: o pagseguro só aceita sedex/pac
+
+    function montaCorreiosURL($source)
+    {
+        $peso = number_format($this->peso, 3, ',', '');
+        $serv = $self::$servicosCorreios[$source];
+        $url  = $self::$urlCorreios;
+        $data = array(
+            'StrRetorno'          => 'xml',
+            'nCdServico'          => $serv,
+            'nVlPeso'             => $peso,
+            'sCepOrigem'          => $this->origem,
+            'sCepDestino'         => $this->destino,
+            'nCdFormato'          => 1,
+            'sCdMaoPropria'       => 'N',
+            'sCdAvisoRecebimento' => 'N',
+            'nVlValorDeclarado'   => $this->valor,
+        );
+    }
 }
